d5d5c0fcd9b6a8ad30c5137dfe1db15972d107a5
Riley <rmcaldwell2015@gmail.com>
2026-03-25 22:39:51 -0400

    SEO Fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
+  // UPDATE THIS FOR EVERY NEW CLIENT
   site: "https://www.sweetcarolineseaport.com",
   output: "static",
   vite: {
