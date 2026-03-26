ae2afb265ffd44aed2d29115f48dbe8993d4f576
Riley <rmcaldwell2015@gmail.com>
2026-03-26 13:38:38 -0400

    more

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   // UPDATE THIS FOR EVERY NEW CLIENT
-  site: "https://www.sweetcarolineseaport.com",
+  site: "https://sweetcarolineseaport.com",
   trailingSlash: 'ignore',
   output: "static",
   vite: {
